abd854717869b57e99d98062aac12c4ffc2c8628
Richard Hooper <richard@frumious.co.uk>
2022-12-13 00:11:20 +0000

    Day 11a - working with real input

--- 11/advent11a.py
+++ 11/advent11a.py
@@ -37,10 +37,14 @@ def main():
     ROUND_COUNT = 20
 
     monkeys = {}
-    monkeys[0] = Monkey([79, 98], lambda i: i * 19, 23, 2, 3)
-    monkeys[1] = Monkey([54, 65, 75, 74], lambda i: i + 6, 19, 2, 0)
-    monkeys[2] = Monkey([79, 60, 97], lambda i: i * i, 13, 1, 3)
-    monkeys[3] = Monkey([74], lambda i: i + 3, 17, 0, 1)
+    monkeys[0] = Monkey([91, 54, 70, 61, 64, 64, 60, 85], lambda i: i * 13, 2, 5, 2)
+    monkeys[1] = Monkey([82], lambda i: i + 7, 13, 4, 3)
+    monkeys[2] = Monkey([84, 93, 70], lambda i: i + 2, 5, 5, 1)
+    monkeys[3] = Monkey([78, 56, 85, 93], lambda i: i * 2, 3, 6, 7)
+    monkeys[4] = Monkey([64, 57, 81, 95, 52, 71, 58], lambda i: i * i, 11, 7, 3)
+    monkeys[5] = Monkey([58, 71, 96, 58, 68, 90], lambda i: i + 6, 17, 4, 1)
+    monkeys[6] = Monkey([56, 99, 89, 97, 81], lambda i: i + 1, 7, 0, 2)
+    monkeys[7] = Monkey([68, 72], lambda i: i + 8, 19, 6, 0)
 
     for round in range(1, ROUND_COUNT + 1):
         print(f"Round {round}")
